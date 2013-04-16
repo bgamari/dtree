d656655f17f36f77b8a396865e7a31e50118c4f9
Ben Gamari <bgamari.foss@gmail.com>
2013-04-16 19:24:57 -0400

    Move things over from MinIR

---
+++ src/Data/DIntMap.hs
@@ -0,0 +1,260 @@
+{-# LANGUAGE DeriveGeneric, FlexibleContexts, FlexibleInstances, TypeSynonymInstances #-}
+
+module Data.DIntMap ( DIntMap
+                    , MemTree, DiskTree
+                    , empty
+                    , insert, insertWith
+                    , lookup
+                    , writeTree
+                    , getRoot, putRoot
+                    , new
+                    , open
+                    , showTree
+                    ) where
+
+import Prelude hiding (mapM, mapM_, lookup, concatMap)
+import Data.Binary
+import Data.Binary.Get
+import Data.Binary.Put
+import Data.Hashable
+import Data.Foldable
+import Data.Traversable
+import Data.Monoid
+import Control.Applicative hiding (empty)
+import Control.Monad (void, replicateM, when)
+import qualified Data.IntMap.Strict as IM
+import Data.IntMap (Key)
+
+import qualified Data.DiskStore as DS
+
+maxBreadth = 1024
+
+data Bin a = Bin !(IM.IntMap a) !a
+           deriving (Show)
+
+instance Functor Bin where
+    fmap f (Bin rest last) = Bin (fmap f rest) (f last)
+instance Foldable Bin where
+    foldMap f (Bin rest last) = foldMap f rest `mappend` f last
+instance Traversable Bin where
+    traverse f (Bin rest last) = Bin <$> traverse f rest <*> f last
+
+lookupBin :: Int -> Bin a -> a
+lookupBin k (Bin rest last) =
+    case IM.lookupGT k rest of
+      Nothing     -> last
+      Just (_,b)  -> b
+
+insertBin :: Int -> a -> Bin a -> Bin a
+insertBin k v (Bin rest last) = Bin (IM.insert k v rest) last
+
+updateBin :: (a -> a) -> Int -> Bin a -> Bin a
+updateBin f k (Bin rest last) =
+    case IM.lookupGT k rest of
+      Just (k',b) -> Bin (IM.adjust f k' rest) last
+      Nothing     -> Bin rest (f last)
+
+binMinKey :: Bin a -> Maybe Key
+binMinKey (Bin rest _)
+    | IM.null rest  = Nothing
+    | otherwise     = let (k,_) = IM.findMin rest in Just k
+
+binSize :: Bin a -> Int
+binSize (Bin rest _) = IM.size rest + 1
+
+-- Children keys are greater than or equal to container's key
+data Tree v = Leaf { _tNValues :: !Int
+                   , _tValues :: !(IM.IntMap v) }
+            | Internal { _tNChildren :: !Int
+                       , _tChildren :: !(Bin (ObjUpdate (Tree v))) }
+            deriving (Show)
+
+empty :: Tree v
+empty = Leaf 0 IM.empty
+
+minKey :: Tree v -> Maybe Key
+minKey (Leaf _ values)
+    | IM.null values  = Nothing
+    | otherwise       = let (k,_) = IM.findMin values in Just k
+minKey (Internal _ children) = binMinKey children
+
+type DiskTree v = Tree v
+type MemTree v = Tree v
+data ObjUpdate a = RefObj !(DS.Obj a) !(Maybe a)
+                 | NewObj !a
+                 deriving (Show)
+
+commitObjUpdate :: Binary a => DS.DiskStore a -> ObjUpdate a -> IO (DS.Obj a)
+commitObjUpdate _      (RefObj a _) = return a
+commitObjUpdate dstore (NewObj a  ) = DS.appendObj dstore a
+
+instance Binary (ObjUpdate a) where
+    put (RefObj ref _) = put ref
+    put (NewObj _)     = error "DIntMap: Tried to serialize NewObj"
+    get = RefObj <$> get <*> pure Nothing
+
+instance (Binary v) => Binary (DiskTree v) where
+    put (Leaf n values) = do putWord8 0
+                             putWord32le $ fromIntegral n
+                             mapM_ put $ IM.toAscList values
+    put (Internal n (Bin children last)) = do
+        putWord8 1
+        putWord32le $ fromIntegral (n-1)
+        mapM_ put $ IM.toAscList children
+        when (IM.size children /= n-1) $ fail $ "Size incorrect "++show (IM.size children, n)
+        put last
+    get = do type_ <- getWord8
+             case type_ of
+                 0 -> do n <- fromIntegral <$> getWord32le
+                         Leaf n . IM.fromDistinctAscList <$> replicateM n get
+                 1 -> do n <- fromIntegral <$> getWord32le
+                         children <- IM.fromDistinctAscList <$> replicateM n get
+                         last <- get
+                         return $ Internal (n+1) (Bin children last)
+                 i -> fail $ "DIntMap: Unknown tree type ("++show i++")"
+
+data DIntMap v = DIntMap { _store :: DS.DiskStore (DiskTree v) }
+
+lookup :: (Binary v, Show v) => Key -> DIntMap v -> IO (Maybe v)
+lookup k dmap = getRoot dmap >>= lookup' dmap k
+
+lookup' :: (Binary v, Show v) => DIntMap v -> Key -> MemTree v -> IO (Maybe v)
+lookup' dmap k (Leaf _ values) = return $ IM.lookup k values
+lookup' dmap k tree = do
+    child <- readChild dmap k tree
+    case child of
+      Just tree -> lookup' dmap k tree
+      Nothing   -> return Nothing
+
+-- | Divide an IntMap into two pieces of roughly equal size
+divide :: IM.IntMap a -> (IM.IntMap a, IM.IntMap a)
+divide m | IM.null m = (IM.empty, IM.empty)
+divide m = let n = IM.size m
+               (k,_) = head $ drop (n `div` 2) $ IM.toAscList m
+               (a,x,b) = IM.splitLookup k m
+           in (maybe a (\v->IM.insert k v a) x, b)
+
+insert :: (Binary v, Show v) => DIntMap v -> Key -> v -> MemTree v -> IO (MemTree v)
+insert dmap = insertWith dmap (\_ a->a)
+
+data Insert v = Ok !(MemTree v)
+              | Split !Key !(MemTree v) !(MemTree v)
+              deriving (Show)
+
+insertWith :: (Binary v, Show v)
+           => DIntMap v -> (v -> v -> v) -> Key -> v -> MemTree v -> IO (MemTree v)
+insertWith dmap f k v tree = do
+    s <- insertWith' dmap f k v tree
+    case s of
+      Ok tree'  -> return tree'
+      Split kl l r -> return $ Internal 2 $ Bin (IM.singleton kl (NewObj l)) (NewObj r)
+
+insertWith' :: (Binary v, Show v)
+            => DIntMap v -> (v -> v -> v) -> Key -> v -> MemTree v -> IO (Insert v)
+--- We are on a leaf node, insert into it
+insertWith' _ f k v (Leaf n values)
+    -- We need to split
+  | n' > maxBreadth = let (l,r) = divide values'
+                          nl = IM.size l
+                          nr = n' - nl
+                          Just ((kl,_),r') = IM.minViewWithKey r
+                      in if nl /= IM.size l || nr /= IM.size r
+                           then error ("size mismatch "++show (n,nl,nr,n',IM.size values,IM.size values'))
+                           else return $ Split kl (Leaf nl l) (Leaf nr r)
+    -- We have enough room
+  | otherwise      = return $ Ok $ Leaf n' values'
+  where (oldV, values') = IM.insertLookupWithKey (const f) k v values
+        n' = maybe (n+1) (const $ n) oldV
+
+-- We are on an internal node
+insertWith' dmap f k v (Internal n children) = do
+    maybeChild <- readChild dmap k (Internal n children)
+    case maybeChild of
+      Nothing -> error "DIntMap: Internal node not found"
+      Just child -> do
+        insert <- insertWith' dmap f k v child
+        case insert of
+          Ok child'     -> let children' = updateBin (const $ NewObj child') k children
+                           in return $ Ok $ Internal n children'
+          Split kl l r  -> let kr = k
+                               children' = insertBin kl (NewObj l)
+                                         $ updateBin (const $ NewObj r) kr
+                                         $ children
+                               n' = binSize children'
+                           in return $ splitInternal $ Internal n' children'
+
+  where splitInternal :: Tree v -> Insert v
+        splitInternal (Leaf _ _) = error "DIntMap: splitInternal can't split Leafs"
+        splitInternal (Internal n (Bin children c))
+          | n > maxBreadth =
+            let (l,r) = divide children
+                Just ((kr', r'), _) = IM.minViewWithKey r
+                nl = IM.size l + 1
+                --nr = n - nl -- FIXME
+                nr = IM.size r + 1
+            in Split kr' (Internal nl (Bin l r')) (Internal nr (Bin r c))
+          | otherwise      = Ok $ Internal n (Bin children c)
+
+writeChildren :: (Binary v) => DIntMap v -> MemTree v -> IO (DiskTree v)
+writeChildren _                 (Leaf n values)       = return (Leaf n values)
+writeChildren dmap@(DIntMap store) (Internal n children) =
+    Internal n <$> mapM writeChild children
+  where writeChild (NewObj a) = do written <- writeChildren dmap a
+                                   a' <- DS.appendObj store written
+                                   return $ RefObj a' (Just a)
+        writeChild a = return a
+
+writeTree :: (Binary v) => DIntMap v -> MemTree v -> IO (DiskTree v, DS.Obj (DiskTree v))
+writeTree dmap@(DIntMap store) tree = do
+    tree' <- writeChildren dmap tree
+    obj <- DS.appendObj store tree'
+    return (tree', obj)
+
+-- | Fetch the child node corresponding to the given key (if such node exists)
+readChild :: (Binary v)
+          => DIntMap v -> Key -> MemTree v -> IO (Maybe (MemTree v))
+readChild (DIntMap store) _ (Leaf _ _)          = return Nothing
+readChild (DIntMap store) k (Internal _ children) =
+    case lookupBin k children of
+        (NewObj obj)          -> return $ Just obj
+        (RefObj _ (Just obj)) -> return $ Just obj
+        (RefObj ref Nothing)  -> do obj <- DS.getObj store ref
+                                    return $ Just obj
+
+putRoot :: (Binary v) => DIntMap v -> MemTree v -> IO (DiskTree v)
+putRoot dmap@(DIntMap store) tree = do
+    (tree', obj) <- writeTree dmap tree
+    void $ DS.setRoot store (Just obj)
+    return tree'
+
+getRoot :: (Binary v) => DIntMap v -> IO (MemTree v)
+getRoot (DIntMap store) = maybe (error "DIntMap: Empty root") id <$> DS.getRoot store
+
+showTree :: (Show v) => (Tree v) -> String
+showTree = unlines . go
+  where go (Leaf _ values) =
+            ["leaf size="++show (IM.size values)]++values'
+          where values' = map ("| "++) $ concatMap showKeyValue $ IM.toAscList values
+                showKeyValue (k,v) = ["key="++show k++"  ->  "++show v]
+        go (Internal _ (Bin rest last)) =
+            ["Internal size="++show (1+IM.size rest)]++children'
+          where children' = map ("| "++)
+                            $ concatMap showChild
+                            $ (map (\(k,v)->(show k, v)) $ IM.toAscList rest)++[("last",last)]
+
+        showChild :: (Show v) => (String, ObjUpdate (Tree v)) -> [String]
+        showChild (k, NewObj a)   = ["New child key="++k]++map ("  "++) (go a)
+        showChild (k, RefObj a b) = ["Ref child key="++k++" to "++show a]++child
+          where child = case b of
+                  Nothing -> []
+                  Just c  -> map ("| "++) $ go c
+
+new :: Binary v => FilePath -> IO (DIntMap v)
+new fname = do
+    store <- DS.new fname
+    a <- DS.appendObj store empty
+    void $ DS.setRoot store (Just a)
+    return $ DIntMap store
+
+open :: Binary v => FilePath -> IO (Maybe (DIntMap v))
+open fname = fmap DIntMap <$> DS.open fname
---
+++ src/Data/DiskStore.hs
@@ -0,0 +1,131 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Data.DiskStore ( Obj
+                      , DiskStore
+                      -- * Creating disk stores
+                      , fromHandle
+                      , new
+                      , open
+                      , close
+                      -- * Manipulating the root object
+                      , getRoot
+                      , setRoot
+                      -- * Adding and retriving objects
+                      , getObj
+                      , appendObj
+                      ) where
+
+import Control.Concurrent.MVar
+import Control.Applicative
+import GHC.Generics
+import System.IO
+import System.Directory
+import Control.Monad
+
+import Data.Binary
+import Data.Binary.Put
+import Data.Binary.Get
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
+
+newtype Obj a = Obj Word64
+              deriving (Show, Eq, Ord, Generic)
+
+instance Binary (Obj a)
+
+withReadLock :: DiskStore root -> IO b -> IO b
+withReadLock dstore m = withMVar (readLock dstore) (const m)
+
+-- We need to be very careful here as we use lazy I/O
+-- If the returned structure isn't evaluated completely,
+-- we may attempt to read a block after the file position has
+-- been moved elsewhere.
+getObj :: Binary a => DiskStore root -> Obj a -> IO a
+getObj dstore (Obj offset) = withReadLock dstore $ do
+    hSeek (handle dstore) AbsoluteSeek (fromIntegral offset)
+    a <- BSL.hGetContents (handle dstore)
+    return $! decode a
+
+appendObj :: Binary a => DiskStore root -> a -> IO (Obj a)
+appendObj dstore a = withReadLock dstore $ do
+    let DStore {handle=h} = dstore
+    hSeek h SeekFromEnd 0
+    offset <- hTell h
+    BSL.hPut h $ encode a
+    return $ Obj $ fromIntegral offset
+
+headerMagic = 0xdeadbeef :: Word64
+
+data Header a = Header { magic :: !Word64
+                       , root  :: !(Maybe (Obj a))
+                       }
+              deriving (Generic)
+
+instance Binary (Header a) where
+    put h = do putWord64le (magic h)
+               case root h of
+                 Just a  -> putWord8 1 >> put a
+                 Nothing -> putWord8 0 >> put (Obj 0)
+    get = Header <$> getWord64le <*> get
+
+data DiskStore a = DStore { handle :: !Handle
+                          , readLock :: !(MVar ())
+                          }
+
+headerObj = Obj 0 :: Obj (Header a)
+
+getHeader :: DiskStore a -> IO (Maybe (Header a))
+getHeader dstore = do
+    header <- getObj dstore headerObj
+    if magic header == headerMagic
+      then return $ Just header
+      else return Nothing
+
+putHeader :: DiskStore a -> Header a -> IO ()
+putHeader dstore header = withReadLock dstore $ do
+    let DStore {handle=h} = dstore
+    hSeek h AbsoluteSeek 0
+    BSL.hPut h $ encode header
+
+getRoot :: Binary a => DiskStore a -> IO (Maybe a)
+getRoot dstore = do
+    header <- getHeader dstore
+    case header of
+      Just hdr -> case root hdr of
+                    Just o  -> Just <$> getObj dstore o
+                    Nothing -> return Nothing
+      Nothing  -> fail "DiskStore: Invalid header"
+
+setRoot :: DiskStore a -> Maybe (Obj a) -> IO ()
+setRoot dstore root = do
+    header <- getHeader dstore
+    case header of
+      Just hdr -> putHeader dstore (hdr {root=root})
+      Nothing  -> error "DiskStore: Invalid header"
+
+fromHandle :: Handle -> IO (Maybe (DiskStore a))
+fromHandle h = do
+    readLock <- newMVar ()
+    let dstore = DStore h readLock
+    header <- getHeader dstore
+    case header of
+      Just _  -> return $ Just dstore
+      Nothing -> return Nothing
+
+new :: Binary a => FilePath -> IO (DiskStore a)
+new fname = do
+    exists <- doesFileExist fname
+    when exists $ fail "DiskStore already exists"
+    h <- openBinaryFile fname ReadWriteMode
+    readLock <- newMVar ()
+    let dstore = DStore h readLock
+    putHeader dstore $ Header headerMagic Nothing
+    return dstore
+
+open :: FilePath -> IO (Maybe (DiskStore a))
+open fname = do
+    openBinaryFile fname ReadWriteMode >>= fromHandle
+
+close :: DiskStore a -> IO ()
+close dstore = hClose (handle dstore)
