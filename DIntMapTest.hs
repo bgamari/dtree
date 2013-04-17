cd5836f5fb9a3925cbe170a3a9461b392f37581c
Ben Gamari <bgamari.foss@gmail.com>
2013-04-17 10:50:53 -0400

    DIntMapTest: Make it smaller

--- DIntMapTest.hs
+++ DIntMapTest.hs
@@ -3,13 +3,13 @@ import Control.Monad
 import Data.Hashable
 
 xs :: [Int]
-xs = [0..3000000]
+xs = [0..1000000]
 
 f = hash
 main = do
-    dmap <- DIM.new "hello" :: IO (DIM.DIntMap String)
+    dmap <- DIM.new "hello" :: IO (DIM.DIntMap Int)
     root <- DIM.getRoot dmap
-    root' <- foldM (\t i->DIM.insert dmap (f i) (replicate 256 'a') t) root xs
+    root' <- foldM (\t i->DIM.insert dmap (f i) 1 t) root xs
     DIM.putRoot dmap root'
     --putStrLn $ showTree root'
     --mapM_ (\i->DIM.lookup (f i) dmap >>= print) xs
